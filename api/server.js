b810c496eb32707d23bd48fa9c1db805a531e0a7
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-05-30 20:47:11 +0300

    removed in-mem adapter

--- api/server.js
+++ api/server.js
@@ -1,33 +1,19 @@
-const jsonServer = require('json-server');
-const low = require('lowdb');
-const Memory = require('lowdb/adapters/Memory');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create()
 
-const dbFilePath = path.join(__dirname, 'db.json');
-const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'));
+const router = jsonServer.router('db.json')
 
-const adapter = new Memory();
-const db = low(adapter);
-db.defaults(dbData).write();
+const middlewares = jsonServer.defaults()
 
-const router = jsonServer.router(db);
-
-server.use(cors());
-server.use(middlewares);
+server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}));
-server.use(router);
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log('JSON Server is running on port', port);
-});
-
-module.exports = server;
+module.exports = server
