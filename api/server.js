3bd77fa0f9adaa969696b10edc23632fb5e8eefe
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-05-30 20:37:50 +0300

    for purposes of CRUD, added in-memory adapter and still utilize availabe db.json

--- api/server.js
+++ api/server.js
@@ -1,27 +1,31 @@
 const jsonServer = require('json-server');
 const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
 const Memory = require('lowdb/adapters/Memory');
+const fs = require('fs');
+const path = require('path');
 
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
+const dbFilePath = path.join(__dirname, 'db.json');
+const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'));
+
 const adapter = new Memory();
 const db = low(adapter);
-
-db.defaults({ invoices: [], collections: [], schools: [] }).write();
+db.defaults(dbData).write();
 
 const router = jsonServer.router(db);
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }));
 server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log('JSON Server is running on port', port);
 });
 
 module.exports = server;
